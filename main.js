c85fd330278082e3c2ccfc85bb95bc97f8b12b4e
ashsic <99445200+ashsic@users.noreply.github.com>
2023-12-27 22:54:21 -0500

    Complete game can be played, replay enabled Mostly done here, can style further or add features

--- main.js
+++ main.js
@@ -45,15 +45,6 @@ const displayController = (function () {
 
     const htmlBoard = document.querySelectorAll(".square");
 
-    const resetBoard = () => {
-        htmlBoard.forEach((square) => {
-            square.textContent = ' ';
-            square.addEventListener('click', (event) => {
-                event.target.textContent = playerTurn;
-            });
-        });
-    };
-
     const updateBoard = () => {
         htmlBoard.forEach((square) => {
             square.firstChild.textContent = Gameboard.board[square.getAttribute("id") - 1];
@@ -62,28 +53,21 @@ const displayController = (function () {
 
     const playGame = (playerA, playerB, board) => {
         const narrator = document.querySelector(".narrator");
-        for (let i=0;i<9;i++) {
-            let index;
-            let playerTurn;
-            
+        let i = 0;
+        let playerTurn;
+        let gameOver;
+        narrator.textContent = "Player 1 turn:";
 
-            if (i % 2 == 0) {
+        const squareClickHandler = (event) => {
+        
+            if (i % 2 == 1) {
                 narrator.textContent = "Player 1 turn:";
-                // index = prompt(`Player ${playerA.playerNumber} enter an index: `);
-                // while (board[index] !== ' '){
-                //     index = prompt(`Player ${playerA.playerNumber} enter an index: `);
-                // };
-                // board[index] = playerA.playerMarker;
-                playerTurn = playerA;
-    
+                playerTurn = playerB;
+                board[event.target.getAttribute('id') - 1] = playerB.playerMarker;
             } else {
                 narrator.textContent = "Player 2 turn:";
-                // index = prompt(`Player ${playerB.playerNumber} enter an index: `);
-                // while (board[index] !== ' '){
-                //     index = prompt(`Player ${playerB.playerNumber} enter an index: `);
-                // };
-                // board[index] = playerB.playerMarker;
-                playerTurn = playerB;
+                playerTurn = playerA;
+                board[event.target.getAttribute('id') - 1] = playerA.playerMarker;
             };
     
             updateBoard();
@@ -101,29 +85,47 @@ const displayController = (function () {
                     board[2] === board[4] && board[4] === board[6] && board[6] !== ' ' 
                 )  {
                     narrator.textContent = `Winner! Player ${playerTurn.playerNumber} has won the game!`;
-                    return;
+                    gameOver = true;
+                    htmlBoard.forEach((square) => {
+                        square.removeEventListener('click', squareClickHandler);
+                    });
                 };
             };
-    
+            if (i === 9) narrator.textContent = "It's a tie!";
+            i++;
+            event.target.removeEventListener('click', squareClickHandler);
         };
-        narrator.textContent = "It's a tie!";
+
+        const resetBoard = () => {
+            htmlBoard.forEach((square, i) => {
+                square.removeEventListener('click', squareClickHandler);
+                Gameboard.board[i] = ' ';
+                square.textContent = ' ';
+                square.addEventListener('click', squareClickHandler);
+
+            });
+            
+        };
+        resetBoard();
+
     };
-
-
 
     const startGame = () => {
         const buttonDiv = document.querySelector(".button");
         const startButton = document.createElement("button");
-        startButton.textContent = "Start";
+        startButton.textContent = "Reset";
         buttonDiv.appendChild(startButton);
         startButton.addEventListener('click', (event) => {
-            event.target.textContent = "Reset";
+            for (let i=0;i<9;i++){
+                Gameboard.board[i] = ' ';
+            };
             playGame(player1, player2, Gameboard.board);
         });
     };
 
-    return { htmlBoard, resetBoard, updateBoard, startGame };
 
+
+    return { htmlBoard, updateBoard, startGame, playGame };
 })();
 
 
@@ -133,4 +135,5 @@ const player1 = createPlayer(1, 'X');
 const player2 = createPlayer(2, 'O');
 
 displayController.updateBoard();
+displayController.playGame(player1, player2, Gameboard.board);
 displayController.startGame();
