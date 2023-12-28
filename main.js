4098830c0dcb5e2c33f4b28ecb85f8e3973966cd
ashsic <99445200+ashsic@users.noreply.github.com>
2023-12-27 21:06:26 -0500

    JS DOM edits and IIFE for game controller

--- main.js
+++ main.js
@@ -25,53 +25,8 @@ function createPlayer (number, marker) {
     return { playerNumber, playerMarker };
 };
 
-function playGame (playerA, playerB, board) {
-    for (let i=0;i<9;i++) {
-        let index;
-        let playerTurn;
-        if (i % 2 == 0) {
-            index = prompt(`Player ${playerA.playerNumber} enter an index: `);
-            while (board[index] !== ' '){
-                index = prompt(`Player ${playerA.playerNumber} enter an index: `);
-            };
-            board[index] = playerA.playerMarker;
-            playerTurn = playerA;
-
-        } else {
-            index = prompt(`Player ${playerB.playerNumber} enter an index: `);
-            while (board[index] !== ' '){
-                index = prompt(`Player ${playerB.playerNumber} enter an index: `);
-            };
-            board[index] = playerB.playerMarker;
-            playerTurn = playerB;
-        };
-
-        Gameboard.printBoard();
-
-        if (i >= 4) {
-            if (
-                board[0] === board[1] && board[1] === board[2] && board[2] !== ' ' ||
-                board[3] === board[4] && board[4] === board[5] && board[5] !== ' ' || 
-                board[6] === board[7] && board[7] === board[8] && board[8] !== ' ' ||
-                board[0] === board[3] && board[3] === board[6] && board[6] !== ' ' ||
-                board[1] === board[4] && board[4] === board[7] && board[7] !== ' ' ||
-                board[2] === board[5] && board[5] === board[8] && board[8] !== ' ' ||
-                board[0] === board[4] && board[4] === board[8] && board[8] !== ' ' ||
-                board[2] === board[4] && board[4] === board[6] && board[6] !== ' ' 
-            )  {
-                console.log(`Winner! Player ${playerTurn.playerNumber} has won the game!`);
-                return;
-            }
-        };
 
 
-
-    };
-    console.log("It's a tie!");
-};
-
-const player1 = createPlayer(1, 'X');
-const player2 = createPlayer(2, 'O');
 
 // while (true) {
 //     playGame(player1, player2, Gameboard.board);
@@ -87,12 +42,95 @@ const player2 = createPlayer(2, 'O');
 
 
 const displayController = (function () {
-    const updateBoard = () => {
-        let displayBoard = document.querySelectorAll(".square");
-        displayBoard.forEach((square) => {
-            square.textContent = Gameboard.board[square.id + 1];
+
+    const htmlBoard = document.querySelectorAll(".square");
+
+    const resetBoard = () => {
+        htmlBoard.forEach((square) => {
+            square.textContent = ' ';
+            square.addEventListener('click', (event) => {
+                event.target.textContent = playerTurn;
+            });
         });
     };
 
-    return { updateBoard }
+    const updateBoard = () => {
+        htmlBoard.forEach((square) => {
+            square.firstChild.textContent = Gameboard.board[square.getAttribute("id") - 1];
+        });
+    };
+
+    const playGame = (playerA, playerB, board) => {
+        const narrator = document.querySelector(".narrator");
+        for (let i=0;i<9;i++) {
+            let index;
+            let playerTurn;
+            
+
+            if (i % 2 == 0) {
+                narrator.textContent = "Player 1 turn:";
+                // index = prompt(`Player ${playerA.playerNumber} enter an index: `);
+                // while (board[index] !== ' '){
+                //     index = prompt(`Player ${playerA.playerNumber} enter an index: `);
+                // };
+                // board[index] = playerA.playerMarker;
+                playerTurn = playerA;
+    
+            } else {
+                narrator.textContent = "Player 2 turn:";
+                // index = prompt(`Player ${playerB.playerNumber} enter an index: `);
+                // while (board[index] !== ' '){
+                //     index = prompt(`Player ${playerB.playerNumber} enter an index: `);
+                // };
+                // board[index] = playerB.playerMarker;
+                playerTurn = playerB;
+            };
+    
+            updateBoard();
+            Gameboard.printBoard();
+    
+            if (i >= 4) {
+                if (
+                    board[0] === board[1] && board[1] === board[2] && board[2] !== ' ' ||
+                    board[3] === board[4] && board[4] === board[5] && board[5] !== ' ' || 
+                    board[6] === board[7] && board[7] === board[8] && board[8] !== ' ' ||
+                    board[0] === board[3] && board[3] === board[6] && board[6] !== ' ' ||
+                    board[1] === board[4] && board[4] === board[7] && board[7] !== ' ' ||
+                    board[2] === board[5] && board[5] === board[8] && board[8] !== ' ' ||
+                    board[0] === board[4] && board[4] === board[8] && board[8] !== ' ' ||
+                    board[2] === board[4] && board[4] === board[6] && board[6] !== ' ' 
+                )  {
+                    narrator.textContent = `Winner! Player ${playerTurn.playerNumber} has won the game!`;
+                    return;
+                };
+            };
+    
+        };
+        narrator.textContent = "It's a tie!";
+    };
+
+
+
+    const startGame = () => {
+        const buttonDiv = document.querySelector(".button");
+        const startButton = document.createElement("button");
+        startButton.textContent = "Start";
+        buttonDiv.appendChild(startButton);
+        startButton.addEventListener('click', (event) => {
+            event.target.textContent = "Reset";
+            playGame(player1, player2, Gameboard.board);
+        });
+    };
+
+    return { htmlBoard, resetBoard, updateBoard, startGame };
+
 })();
+
+
+
+
+const player1 = createPlayer(1, 'X');
+const player2 = createPlayer(2, 'O');
+
+displayController.updateBoard();
+displayController.startGame();
